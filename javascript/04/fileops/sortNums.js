de89ba05be311a0875f69ae0beeb0a0769cd8545
Arnav Gupta <championswimmer@gmail.com>
2022-06-24 22:07:17 +0530

    both assignments done
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- javascript/04/fileops/sortNums.js
+++ javascript/04/fileops/sortNums.js
@@ -1,8 +1,15 @@
 const fs = require('fs');
 
-/**
+/** BASIC
  * 1. read the numbers from file1.txt
  * 2. sort them in ascending order
+ * 3. write the sorted numbers to out.txt
+ */
+
+/** ADVANCED
+ * 1. read the numbers from file1.txt
+ * 2. read the numbers from file2.txt
+ * 2. sort the combined list of all numbers in ascending order
  * 3. write the sorted numbers to out.txt
  */
 
