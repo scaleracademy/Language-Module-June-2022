1f2bb4d04eb46476fcdacf53b80797c25324a1c6
Sujay Alaspure <31307865+alaspuresujay@users.noreply.github.com>
2022-06-24 22:28:58 +0530

    Create sortNums.js

--- javascript/04/fileops/sortNums.js
+++ javascript/04/fileops/sortNums.js
@@ -5,6 +5,26 @@ const fs = require('fs');
  * 2. sort them in ascending order
  * 3. write the sorted numbers to out.txt
  */
+let arr = [];
+fs.readFile(`${__dirname}/file1.txt`, (err, data) => {
+  if (err) throw err;
+
+  console.log("org", data.toString().split("\n"));
+  arr = data.toString().split("\n");
+  arr.sort((a, b) => a - b);
+  let str = arr.join("\n");
+  console.log("new", str);
+  fs.writeFile(`${__dirname}/out.txt`, str, (err) => {
+    if (err) throw err;
+
+    console.log(" write success");
+  });
+});
+
+fs.close();
+
+
+
 
 /** ADVANCED
  * 1. read the numbers from file1.txt
