e621c7559a23f8a1375ff491c0e26f4768d517bf
Arnav Gupta <championswimmer@gmail.com>
2022-06-27 23:23:38 +0530

    fan out fan in simple code
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- java/04/threads/src/main/java/org/example/FanOutFanIn.java
+++ java/04/threads/src/main/java/org/example/FanOutFanIn.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class FanOutFanIn {
 
@@ -13,37 +15,53 @@ public class FanOutFanIn {
      * -> do this using only threads (no executor, no forkjoinpool etc)
      */
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         new FanOutFanInJob(50, 4).run();
     }
 
     static class FanOutFanInJob {
         ArrayList<Integer> items;
         ArrayList<Thread> threads;
+        int i;
 
         public FanOutFanInJob(int count, int threads) {
             this.items = new ArrayList<Integer>(count);
-            this.threads = new ArrayList<>(threads);
+            this.threads = new ArrayList<>(4);
 
             for (int i = 1; i <= count; i++) {
                 this.items.add(i);
-            }
-            for (int i = 0; i < threads; i++) {
-                this.threads.add(new Thread());
             }
         }
 
         private void printItem(int index) {
             System.out.println(Thread.currentThread().getName() + "  :  " + index);
         }
-
-        void run() {
-            for (int i = 0; i < 10; i++) {
+        void run() throws InterruptedException {
+            i = 0;
+            while (i <= 10) {
                 printItem(i);
+                i++;
             }
             // print 11 to 40 using 4 threads
+            for (int j = 0; j < 4; j++) {
+                threads.add(new Thread(() -> {
+                    while (i <= 40) {
+                        printItem(i);
+                        i++;
+                    }
+                }));
+            }
+
+            threads.forEach(Thread::start);
+            for (Thread thread : threads) {
+                thread.join();
+            }
 
             // print 41-50 using single thread again
+            while (i <= 50) {
+                printItem(i);
+                i++;
+            }
         }
     }
 
